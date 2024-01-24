6a32389eb4dce76d465d28f0c918beefb5d19f32
Jan Heinz <heinz.j@northeastern.edu>
2024-01-24 17:21:25 -0500

    Revert "3.1.3 physics in unity"
    This reverts commit 12f094f38b47bfa9d76b8be11b303ad1b713c074.

--- Assets/CollectionGame_Assets/Scripts/PlayerController.cs
+++ Assets/CollectionGame_Assets/Scripts/PlayerController.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-    Rigidbody rigidBody;
+public class PlayerController : MonoBehaviour
+{
     // Start is called before the first frame update
     void Start()
     {
         // get a reference to the rigidbody component
-        rigidBody = GetComponent<Rigidbody>();
-        
-        rigidBody.AddForce(transform.forward * 5);
     }
 
     // Update is called once per frame
