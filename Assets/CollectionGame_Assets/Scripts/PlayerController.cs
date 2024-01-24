131c39124e02223d8ec23eac4e9fad65e3bf47fb
Jan Heinz <heinz.j@northeastern.edu>
2024-01-24 17:35:09 -0500

    3.1.4 player movement

--- Assets/CollectionGame_Assets/Scripts/PlayerController.cs
+++ Assets/CollectionGame_Assets/Scripts/PlayerController.cs
@@ -1,20 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    public float speed = 2f;
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
         // get a reference to the rigidbody component
+        rigidBody = GetComponent<Rigidbody>();
+        //rigidBody.AddForce(transform.forward * 5);
     }
 
     // Update is called once per frame
     void Update() {
+        
+    }
+
+    private void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-    }
+
+        Vector3 forceVector = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidBody.AddForce(forceVector * speed);
+    } 
+    
 
     void ControlPlayer()
     {
