8b4795b3186cd20ad8d7d3c535854b4b0545e620
Jan Heinz <heinz.j@northeastern.edu>
2024-01-25 17:24:41 -0500

    3.2.1 camera control

--- Assets/CollectionGame_Assets/Scripts/CameraController.cs
+++ Assets/CollectionGame_Assets/Scripts/CameraController.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
+
+    GameObject player;
+    Vector3 offSet;
+    
     // Start is called before the first frame update
     void Start()
     {
         // get and store the offset from the camera to the player
+        player = GameObject.FindGameObjectWithTag("Player");
+        offSet = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         // add the offset to the player's position
+        transform.position = player.transform.position + offSet;
         // assign it to the camera's position
     }
 }
