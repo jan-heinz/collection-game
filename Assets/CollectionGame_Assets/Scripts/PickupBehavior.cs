9eb55731943536c24fde1124c45f5affe271b175
Jan Heinz <heinz.j@northeastern.edu>
2024-01-25 13:10:53 -0500

    3.1.5 collision detection

--- Assets/CollectionGame_Assets/Scripts/PickupBehavior.cs
+++ Assets/CollectionGame_Assets/Scripts/PickupBehavior.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickupBehavior : MonoBehaviour
-{
+public class PickupBehavior : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log(collision.gameObject.name + " hit me!");
+        // Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log(other.gameObject.name + " triggered me");
     }
 }
+
